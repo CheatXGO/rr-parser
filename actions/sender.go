d8e64188c7a515a6126479e19ad731970aa9bbcb
CheatXGO <cheatx2007@gmail.com>
2021-09-17 16:11:23 +0300

    Fixed error when user edit message

--- actions/sender.go
+++ actions/sender.go
@@ -9,20 +9,22 @@ import (
 )
 
 func Sender(text string, chatID int, cfg structures.Config) error {
-	reqBody := structures.SendMessage{Id: chatID, Text: text}
-	reqBytes, err := json.Marshal(reqBody)
-	if err != nil {
-		e := structures.MyError{Fun: "json.Marshal &reqBody", Err: "can't encode answer for user"}
-		log.Fatalln(e.Error())
-	}
-	res, err := http.Post("https://api.telegram.org/bot"+cfg.TelegramBotToken+"/sendMessage", "application/json", bytes.NewBuffer(reqBytes))
-	if err != nil {
-		e := structures.MyError{Fun: "http.Post", Err: "can't send answer"}
-		log.Fatalln(e.Error())
-	}
-	if res.StatusCode != http.StatusOK {
-		e := structures.MyError{Fun: "http.StatusOK", Err: "answer send but not OK"}
-		log.Fatalln(e.Error())
+	if chatID != 0 { //check for user-edited messages
+		reqBody := structures.SendMessage{Id: chatID, Text: text}
+		reqBytes, err := json.Marshal(reqBody)
+		if err != nil {
+			e := structures.MyError{Fun: "json.Marshal &reqBody", Err: "can't encode answer for user"}
+			log.Fatalln(e.Error())
+		}
+		res, err := http.Post("https://api.telegram.org/bot"+cfg.TelegramBotToken+"/sendMessage", "application/json", bytes.NewBuffer(reqBytes))
+		if err != nil {
+			e := structures.MyError{Fun: "http.Post", Err: "can't send answer"}
+			log.Fatalln(e.Error())
+		}
+		if res.StatusCode != http.StatusOK {
+			e := structures.MyError{Fun: "http.StatusOK", Err: "answer send but not OK"}
+			println(e.Error(), chatID, text)
+		}
 	}
 	return nil
 }
